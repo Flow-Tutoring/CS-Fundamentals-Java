6f2ee51c036f84bc859845b264eaf9cc4eca922f
taye beckford <tayebeckford@icloud.com>
2023-01-26 23:51:23 -0500

    finished practice double commit

---
+++ solution.java
@@ -0,0 +1,9 @@
+public class solution {
+  public static void main(String[] args) {
+    System.out.println("Hello World!!!");
+    
+    System.out.println("This is the solution");
+
+    System.out.println("I love coding!");
+  }
+}
